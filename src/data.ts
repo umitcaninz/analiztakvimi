ba23897a8d54bb2d1fc2b72f4c0da8f2fed7bc97
Ümit Can İNÖZÜ <107713484+umitcaninz@users.noreply.github.com>
2025-03-13 14:47:03 +0300

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -8,8 +8,8 @@ export const calendarData: DataStore = {
       "is_new": true,
       "link":"https://analiztakvimi.netlify.app/"
     },
-    "2023-09-15": {
-      "date": "2023-09-15",
+    "2025-01-01": {
+      "date": "2025-01-01",
       "description": "Etkinlik bulunmamaktadır.",
       "is_new": false
     }
